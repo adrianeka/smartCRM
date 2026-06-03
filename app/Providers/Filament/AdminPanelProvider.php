b22d64a39fc330991e33cbdb90b38d70d119c60f
Afzzal Faizal Rabby <afzzalfaizalrabby@student.telkomuniversity.ac.id>
2026-06-03 22:54:12 +0700

    Update Filament auth pages: Change brand to SmartCRM and link to homepage

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->brandName('SmartCRM')
             ->databaseNotifications()
             ->databaseNotificationsPolling('30s')
             ->colors([
